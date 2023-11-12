b216446f7c937703d77b7ffa32eac0c52f234474
Nuo Nuo <leewanchien2624@gmail.com>
2023-11-12 14:00:52 +0800

    commit admin

--- admin.py
+++ admin.py
@@ -1,24 +1,23 @@
-#register part
-#tutor
-print("Admin Page")
-registerWho = int(input("1. Register Receptionist \n2. Register Tutor"))
-if registerWho == 1:
-    print("Register Receptionist")
-    receptionistID = int(input("Receptionist ID: "))
-    email = input("Email: ")
-    username = input("Username: ")
-    password = input("Password: ")
-    receptionistString = f"{receptionistID};RECEPTIONIST;{email};{username};{password};"
-elif registerWho:
-    print("Register ")
-    tutorID = int(input("Tutor ID: "))
-    email = input("Email: ")
-    username = input("Username: ")
-    password = input("Password: ")
-    tutorString = f"{tutorID};TUTOR;{email};{username};{password};"
+def dminFile(filename,IDNumber,ICNUmber,fullName,contactNumber,gender,birthday)
 
-def inputToFile(filename,string):
-    with open (filename,"a"):
-        filename.append()
-
+def pgAdmin(userID):
+    while True:
+        generalUtils.clearConsole()
+        generalUtils.createNewLine()
+        with open("UserDetails.txt", "+r") as file:
+            for line in file:
+                values = line.strip().split(";")
+                if values[1] == userID:
+                    splittedName = values[4].replace("_", " ")
+                    print(f"Welcome, {splittedName} ({values[0]})")
+            generalUtils.createNewLine()
+            while True:
+            print("Menu:\n1.Register\n2.Fill Income\n3.Admin Profile")
+            choice = int(input("1.Register\n2.Fill Income\n3.Admin Profile\nChoice:"))
+            if choice == 1:
+                print("Register:\n1.Receptionist\n2.Tutor")
+                choiceRegister = int(input("1.Receptionist\n2.Tutor\nChoice:"))
+                if choiceRegister == 1:
+                    email = input("Email:")
+                    password = input("Password:")
 
