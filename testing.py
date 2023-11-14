35a50e4c8b8fd928db0d767a616e5dc45feb0d9b
hueyshin04 <148762211+hueyshin04@users.noreply.github.com>
2023-11-14 10:54:57 +0800

    student

--- testing.py
+++ testing.py
@@ -1,28 +1,37 @@
 import database
 import generalUtils
 
-def update_profile(student,name,ID,password,IC_num,birthday,email):
-    updated_data= []
 
-    with open(student,"r") as file:
-        line= [line.rstrip() for line in file]
+def update_profile(student, name, ID, password, IC_num, birthday, email):
+    updated_data = []
+
+    with open(student, "r") as file:
+        line = [line.rstrip() for line in file]
         for line in line:
             student_data = line.split(";")
-            if student_data and student_data[1] ==str(ID):
+            if student_data and student_data[1] == str(ID):
                 updated_data.append(f"{name};{ID};{password};{IC_num};{birthday};{email}\n")
             else:
                 updated_data.append(line)
 
-
-    with open(student,"w") as file:
-        file.writelines(updated_data)
-        name = input("Please enter your name:")
-        password = input("Please enter your password:")
-        IC_num = input("Please enter your IC num:")
-        birthday= input("Please enter your birthday:")
-        email=input("Please enter your email:")
+    with open(student, "w") as file:
         file.writelines(updated_data)
 
-student_file= "User.Details.txt"
-update_profile(student_file, name, ID, password, IC_num,birthday,email)
 
+
+ID = input("ID Number: ")
+name = input("Please enter your name:")
+password = input("Please enter your password:")
+IC_num = input("Please enter your IC num:")
+email = input("Please enter your email:")
+while True:
+    birthday = input("Please enter your birthday(DD/MM/YYYY):")
+    birthday = birthday.replace("/","-")
+    print(len(str(birthday)))
+    if len(str(birthday))==10:
+        break
+    else:
+        continue
+
+student_file = "userDetails.txt"
+update_profile(student_file, name, ID, password, IC_num, birthday, email)
